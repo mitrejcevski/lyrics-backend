e8e0f65e1c8a3d1d1c14805b1956139f518eff9b
Jovche Mitrejchevski <mitrejcevski@gmail.com>
2020-06-18 16:07:02 +0200

    Creating new songs completed

--- src/test/java/integration/IT_UsersApi.java
+++ src/test/java/integration/IT_UsersApi.java
@@ -2,6 +2,9 @@ package integration;
 
 import com.eclipsesource.json.JsonObject;
 import integration.data.IT_SongData;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.UUID;
@@ -13,19 +16,28 @@ import static org.hamcrest.Matchers.*;
 
 public class IT_UsersApi {
 
-    private static final String USER_ID = UUID.randomUUID().toString();
-    private static final String GUEST_ID = UUID.randomUUID().toString();
     private static final IT_SongData BEAUTIFUL_BY_AKON = new IT_SongData("Beautiful", "Akon", "Beautiful song lyrics...");
     private static final IT_SongData YEAH_BY_USHER = new IT_SongData("Yeah yeah", "Usher", "Yeah yeah song lyrics...");
     private static final IT_SongData ALWAYS_BY_BON_JOVI = new IT_SongData("Always", "Bon Jovi", "Always song lyrics...");
+
+    private static String USER_ID = UUID.randomUUID().toString();
+    private static String GUEST_ID = UUID.randomUUID().toString();
+
+    @Before
+    public void initialize() {
+        String userId = register("user", "user", "user");
+        if (!userId.equals("")) USER_ID = userId;
+        String guestId = register("guest", "guest", "guest");
+        if (!guestId.equals("")) GUEST_ID = guestId;
+    }
 
     @Test
     public void create_a_song() {
         given()
                 .body(withJsonContainingSongData(BEAUTIFUL_BY_AKON))
-        .when()
+                .when()
                 .post(BASE_URL + "/users/" + USER_ID + "/songs")
-        .then()
+                .then()
                 .statusCode(201)
                 .contentType("application/json")
                 .body("userId", is(USER_ID))
@@ -48,7 +60,7 @@ public class IT_UsersApi {
                 .post(BASE_URL + "/users/" + GUEST_ID + "/songs");
         given()
                 .get(BASE_URL + "/users/" + USER_ID + "/songs")
-        .then()
+                .then()
                 .statusCode(200)
                 .contentType("application/json")
                 .body("songs", hasSize(2))
@@ -65,5 +77,24 @@ public class IT_UsersApi {
                 .add("songPerformer", songData.performer())
                 .add("songLyrics", songData.lyrics())
                 .toString();
+    }
+
+    private String withJsonContaining(String username, String password, String about) {
+        return new JsonObject()
+                .add("username", username)
+                .add("password", password)
+                .add("about", about)
+                .toString();
+    }
+
+    private String register(String username, String password, String about) {
+        RequestSpecification httpRequest = given().body(withJsonContaining(username, password, about));
+        Response response = httpRequest.post(BASE_URL + "/users");
+        return response.statusCode() < 400 ? userIdFrom(response.body().asString()) : "";
+    }
+
+    private String userIdFrom(String json) {
+        JsonObject jsonObject = JsonObject.readFrom(json);
+        return jsonObject.get("id").asString();
     }
 }
